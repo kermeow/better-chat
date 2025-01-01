08398ce338987bcdad35f2b677b1dce063b55364
jayden <46307433+kermeow@users.noreply.github.com>
2025-01-01 03:27:18 +0000

    feat: main.lua

---
+++ main.lua
@@ -0,0 +1,8 @@
+-- name: Better Chat
+-- description: \\#ffff33\\-- Better Chat v1 --\n\n\\#dcdcdc\\Improves on the chat with like emojis n shit :sunglasses:\n\nCreated by: \\#646464\\kermeow\\#dcdcdc\\ & \\#008800\\Squishy6094\n\n\\#aaaaff\\Updates can be found on\nBetter Chat's Github:\n\\#6666ff\\kermeow/better-chat-coop
+
+--[[
+	▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+	█░░░░░░░░▀█▄▀▄▀██████░▀█▄▀▄▀██████░
+	░░░░░░░░░░░▀█▄█▄███▀░░░ ▀██▄█▄███▀░
+--]]
